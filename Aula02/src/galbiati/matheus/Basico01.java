efa58af1c95a61e9474e5fe680b9f8213c140871
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-03-01 12:55:34 -0300

    Modificações Aula02 - Lab

---
+++ Aula02/src/galbiati/matheus/Basico01.java
@@ -0,0 +1,25 @@
+package galbiati.matheus;
+
+public class Basico01 {
+    public static void main(String[] args) {
+        //Tipos primitivos
+        int numero;
+        float hp;
+        double atk;
+        boolean gigantamax;
+        String nome;
+
+        //Atribuição para as variáveis
+        numero = 25;
+        hp = 155.0f;
+        atk = 255.0;
+        gigantamax = true;
+        nome = "Pikachu";
+
+        //Exibe os valores das variáveis
+        System.out.println("Nome: " + nome);
+        System.out.println("Gigantamax: " + gigantamax);
+        System.out.println("HP: " + hp + " ATK: " + atk);
+
+    }
+}
---
+++ Aula02/src/galbiati/matheus/Basico02.java
@@ -0,0 +1,36 @@
+package galbiati.matheus;
+
+public class Basico02 {
+    //Estruturas de decisão
+    public static void main(String[] args) {
+        int ano = 2021;
+
+        //Estrutura de decisão simples
+        if(ano % 2 == 0){
+            System.out.println("Ano par!");
+        }
+        if(ano % 2 == 1){
+            System.out.println("Ano ímpar!");
+        }
+
+        //Estrutura de decisão composta
+        if(ano % 2 == 0){
+            System.out.println("Ano par!");
+        } else{
+            System.out.println("Ano ímpar!");
+        }
+
+        //Varias decisoes
+        switch (ano){
+            case 2020:
+                System.out.println("Vixi!");
+                break;
+            case 1996:
+                System.out.println("Pokemon Red!!");
+                break;
+            default:
+                System.out.println("Quase Boku no Hero saindo!");
+                break;
+        }
+    }
+}

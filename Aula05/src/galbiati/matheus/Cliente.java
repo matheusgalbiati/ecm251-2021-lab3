62e6252526b4e587cbff726e4522daa3b2aec712
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-03-15 13:09:52 -0300

    Aula05 - Lab

---
+++ Aula05/src/galbiati/matheus/Cliente.java
@@ -0,0 +1,18 @@
+package galbiati.matheus;
+
+public class Cliente {
+    private String titular;
+    private String cpf;
+
+    public Cliente(String titular) {
+        this.titular = titular;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "titular='" + titular + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
---
+++ Aula05/src/galbiati/matheus/Conta.java
@@ -0,0 +1,47 @@
+package galbiati.matheus;
+
+public class Conta {
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(int numero, double saldo, String cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = new Cliente(cliente);
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void depositar(double valor) {
+        //this.saldo = this.saldo + valor;
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferirPara(double valor, Conta destino) {
+        if(this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", saldo=" + saldo +
+                ", cliente=" + cliente.toString() +
+                '}';
+    }
+}

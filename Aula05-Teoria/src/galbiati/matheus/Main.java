3ede6d344e70bd006021d6c181ce6773897be036
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-03-21 18:09:42 -0300

    Validador de CPF e atributos e métodos da classe

---
+++ Aula05-Teoria/src/galbiati/matheus/Main.java
@@ -0,0 +1,9 @@
+package galbiati.matheus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "529.982.247-25";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}

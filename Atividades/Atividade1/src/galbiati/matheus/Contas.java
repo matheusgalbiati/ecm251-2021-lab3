187f1f42ddb7c67cec8b84fb65464a017d07da30
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-04-02 16:10:12 -0300

    Refatoração da classe Contas

--- Atividades/Atividade1/src/galbiati/matheus/Contas.java
+++ Atividades/Atividade1/src/galbiati/matheus/Contas.java
@@ -3,61 +3,72 @@
 
 package galbiati.matheus;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Contas {
+    private Scanner scanner;
+
     protected int idConta;
     protected double saldo;
     protected Usuarios usuarios;
-    protected Transacoes transacoes;
-    private Scanner scanner;
-    private int contador=0;
+    private static int totalDeContas;
+
+    Transacoes transacoes = new Transacoes();
+
 
     public Contas() {
-
+        this.scanner = new Scanner(System.in);
     }
 
-    public Contas(int idConta, double saldo, String nome, String senha, String email) {
+    public Contas(int idConta, double saldo, Usuarios usuarios) {
         this.idConta = idConta;
         this.saldo = saldo;
-        this.usuarios = new Usuarios(nome, senha, email);
+        this.usuarios = usuarios;
     }
 
+    protected Contas criaConta(double saldoInicial, String nome, String senha, String email) {
+//        System.out.println("Informe o nome:");
+//        String nome = scanner.next();
+//        System.out.println("Crie uma senha:");
+//        String senha = scanner.next();
+//        System.out.println("Informe o e-mail:");
+//        String email = scanner.next();
+        Usuarios novoUsuario = new Usuarios(nome, senha, email);
+        Contas.totalDeContas += 1;
+        Contas novaConta = new Contas(Contas.totalDeContas, saldoInicial, novoUsuario);
+        return novaConta;
+    }
 
-    protected double getSaldo(int idConta) {
+    protected double getSaldo() {
         return this.saldo;
     }
 
-    protected void depositar(double valor, Contas destino) {
-        destino.saldo += valor;
+    protected void depositar(double valor) {
+        this.saldo += valor;
     }
 
-    protected boolean sacar(double valor, Contas origem) {
-        if (origem.saldo >= valor) {
-            origem.saldo -= valor;
+    protected boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
             return true;
         }
         return false;
     }
 
-
-
-//    protected boolean transferirPara(double valor, Contas origem, Contas destino) {
-//        if(origem.sacar(valor, origem)) {
-//            destino.depositar(valor, destino);
-//            return true;
-//        }
-//        return false;
-//    }
+    protected boolean transferirPara(Contas destino, double valor) {
+        if(this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
         return "Contas{" +
                 "idConta=" + idConta +
                 ", saldo=" + saldo +
-                ", usuarios=" + usuarios +
+                ", usuarios=" + usuarios.toString() +
                 '}';
     }
 }

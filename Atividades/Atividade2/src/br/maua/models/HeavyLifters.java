1b8bfc12255dd0af7f3b3471dddf02369ecc682a
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-18 17:18:46 -0300

    Criação inicial da classe HeavyLifters

---
+++ Atividades/Atividade2/src/br/maua/models/HeavyLifters.java
@@ -0,0 +1,33 @@
+package br.maua.models;
+
+import br.maua.enums.HorarioSistema;
+import br.maua.enums.TiposMembros;
+
+public class HeavyLifters extends Membros{
+
+    public HeavyLifters(String nomeUsuario, String email, TiposMembros funcao) {
+        super(nomeUsuario, email, funcao);
+    }
+
+    @Override
+    public void apresentar() {
+        System.out.println(toString());
+    }
+
+    @Override
+    public void postarMensagem() {
+        if(Sistema.getHorario() == HorarioSistema.Regular)
+        System.out.println("Podem contar conosco!");
+        if(Sistema.getHorario() == HorarioSistema.Extra)
+            System.out.println("N00b_qu3_n_Se_r3pita.bat");
+    }
+
+    @Override
+    public String toString() {
+        return "HeavyLifters{" +
+                "nomeUsuario='" + nomeUsuario + '\'' +
+                ", email='" + email + '\'' +
+                ", funcao=" + funcao +
+                '}';
+    }
+}

22036d36d967317e2a52fdfc36fa0510c2e14782
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:16:25 -0300

    Documentação da classe Sistema

--- Atividades/Atividade2/src/br/maua/models/Sistema.java
+++ Atividades/Atividade2/src/br/maua/models/Sistema.java
@@ -4,18 +4,26 @@ import br.maua.enums.HorarioSistema;
 
 import java.util.Scanner;
 
+/**
+ * Classe responsável pelo funcionamento do sistema
+ */
 public class Sistema {
     public boolean continuarExecucao;
     public Scanner scanner;
 
     private static HorarioSistema horario = HorarioSistema.Regular;
 
-
+    /**
+     * Método construtor da classe Sistema
+     */
     public Sistema() {
         this.continuarExecucao = true;
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Método que inicializa a execução do sistema e avalia as opções
+     */
     public void executar(){
         while (continuarExecucao){
             exibirMenu();
@@ -24,10 +32,18 @@ public class Sistema {
         }
     }
 
+    /**
+     * Getter do horário do sistema
+     * @return O valor do atributo horario
+     */
     public static HorarioSistema getHorario() {
         return horario;
     }
 
+    /**
+     * Método usado para avaliar o input do usuário
+     * @param opcao Opção que o usuário escolher
+     */
     public void avaliarOpcao(int opcao){
         switch (opcao){
             case 0:
@@ -46,6 +62,9 @@ public class Sistema {
         }
     }
 
+    /**
+     * Menu de exibição inicial
+     */
     public void exibirMenu(){
         System.out.println("Bem-vindo a Mask_Society");
         System.out.println("Horario de trabalho atual: " + horario);

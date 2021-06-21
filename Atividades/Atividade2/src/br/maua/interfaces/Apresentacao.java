5f214edd3e8d435478a9d7ad48a9731bdfe721a3
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:46:35 -0300

    Documentação das interfaces

--- Atividades/Atividade2/src/br/maua/interfaces/Apresentacao.java
+++ Atividades/Atividade2/src/br/maua/interfaces/Apresentacao.java
@@ -1,5 +1,8 @@
 package br.maua.interfaces;
 
+/**
+ * Interface que implementa o método que faz a Apresentação do membro
+ */
 public interface Apresentacao {
     public void apresentar();
 }
--- Atividades/Atividade2/src/br/maua/interfaces/PostarMensagem.java
+++ Atividades/Atividade2/src/br/maua/interfaces/PostarMensagem.java
@@ -1,5 +1,8 @@
 package br.maua.interfaces;
 
+/**
+ * Interface que implementa o método que posta a mensagem do membro
+ */
 public interface PostarMensagem {
     public void postarMensagem();
 }

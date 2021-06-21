a25f7639d4b72e92d45877613136ae3add1eb387
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:38:59 -0300

    Documentação da classe BigBrothers

--- Atividades/Atividade2/src/br/maua/models/BigBrothers.java
+++ Atividades/Atividade2/src/br/maua/models/BigBrothers.java
@@ -3,17 +3,32 @@ package br.maua.models;
 import br.maua.enums.HorarioSistema;
 import br.maua.enums.TiposMembros;
 
+/**
+ * Classe do tipo de função Big Brothers
+ */
 public class BigBrothers extends Membros{
 
+    /**
+     * Método construtor da classe Big Brothers
+     * @param nomeUsuario NOme de usuário do membro
+     * @param email e-mail do menbro
+     * @param funcao Função do membro
+     */
     public BigBrothers(String nomeUsuario, String email, TiposMembros funcao) {
         super(nomeUsuario, email, funcao);
     }
 
+    /**
+     * Método que faz a apresentação do membro
+     */
     @Override
     public void apresentar() {
         System.out.println(toString());
     }
 
+    /**
+     * Método que posta uma mensagem dependendo do horário do sistema
+     */
     @Override
     public void postarMensagem() {
         if(qualHorario() == HorarioSistema.Regular)
@@ -22,6 +37,10 @@ public class BigBrothers extends Membros{
             System.out.println("...");
     }
 
+    /**
+     * Método toString
+     * @return A apresentação dos dados do membro
+     */
     @Override
     public String toString() {
         return "BigBrothers{" +

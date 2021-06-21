b8f546cb61d56655c6c0083d2ef2d4fce54e1391
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 20:23:25 -0300

    Implementação da opção de cadastrar membro

--- Atividades/Atividade2/src/br/maua/models/Sistema.java
+++ Atividades/Atividade2/src/br/maua/models/Sistema.java
@@ -1,7 +1,10 @@
 package br.maua.models;
 
 import br.maua.enums.HorarioSistema;
+import br.maua.enums.TiposMembros;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,6 +15,11 @@ public class Sistema {
     public Scanner scanner;
 
     private static HorarioSistema horario = HorarioSistema.Regular;
+
+    List<MobileMembers> mobileMembersList = new ArrayList<>();
+    List<HeavyLifters> heavyLiftersList = new ArrayList<>();
+    List<ScriptGuys> scriptGuysList = new ArrayList<>();
+    List<BigBrothers> bigBrothersList = new ArrayList<>();
 
     /**
      * Método construtor da classe Sistema
@@ -50,6 +58,11 @@ public class Sistema {
                 System.out.println("Obrigado por utilizar nosso sistema");
                 this.continuarExecucao = false;
                 break;
+            case 1:
+                menuCadastro();
+                int tipoMembro = scanner.nextInt();
+                cadastrarMembro(tipoMembro);
+                break;
             case 3:
                 if(horario == HorarioSistema.Regular) {
                     horario = HorarioSistema.Extra;
@@ -73,5 +86,35 @@ public class Sistema {
         System.out.println("3 - Trocar horario de trabalho");
         System.out.println("4 - Excluir membro");
         System.out.println("0 - Encerrar sistema");
+    }
+
+    /**
+     * Menu com as opções de membros para fazer o cadastro
+     */
+    public void menuCadastro(){
+        System.out.println("Escolha o tipo de membro que deseja cadastrar");
+        System.out.println("1 - Mobile Members");
+        System.out.println("2 - Heavy Lifters");
+        System.out.println("3 - Script Guys");
+        System.out.println("4 - Big Brothers");
+    }
+
+    /**
+     * Método que cadastra um novo membro
+     * @param tipoMembro tipo do membro escolhido pelo usuário para cadastrar
+     */
+    public void cadastrarMembro(int tipoMembro){
+        System.out.println("Digite o nome do novo membro:");
+        String nomeNovoMembro = scanner.next();
+        System.out.println("Digite o e-mail do novo membro:");
+        String emailNovoMembro = scanner.next();
+        if(tipoMembro == 1)
+            mobileMembersList.add(new MobileMembers(nomeNovoMembro, emailNovoMembro, TiposMembros.MobileMembers));
+        if(tipoMembro == 2)
+            heavyLiftersList.add(new HeavyLifters(nomeNovoMembro, emailNovoMembro, TiposMembros.HeavyLifters));
+        if(tipoMembro == 3)
+            scriptGuysList.add(new ScriptGuys(nomeNovoMembro, emailNovoMembro, TiposMembros.ScriptGuys));
+        if(tipoMembro == 4)
+            bigBrothersList.add(new BigBrothers(nomeNovoMembro, emailNovoMembro, TiposMembros.BigBrothers));
     }
 }

7fa26af50b79d757bd22088a804078df61396e1c
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:21:09 -0300

    Documentação da classe abstrata Membros

--- Atividades/Atividade2/src/br/maua/models/Membros.java
+++ Atividades/Atividade2/src/br/maua/models/Membros.java
@@ -5,17 +5,30 @@ import br.maua.enums.TiposMembros;
 import br.maua.interfaces.Apresentacao;
 import br.maua.interfaces.PostarMensagem;
 
+/**
+ * Classe abstrata usada como modelo para os tipos de membros
+ */
 abstract class Membros implements PostarMensagem, Apresentacao {
     public String nomeUsuario;
     public String email;
     public TiposMembros funcao;
 
+    /**
+     * Método construtor da classe abstrada Membros
+     * @param nomeUsuario Nome de usuário do membro
+     * @param email e-mail do membro
+     * @param funcao Função atribuída ao membro
+     */
     public Membros(String nomeUsuario, String email, TiposMembros funcao) {
         this.nomeUsuario = nomeUsuario;
         this.email = email;
         this.funcao = funcao;
     }
 
+    /**
+     * Método usado para pegar o valor do horário do sistema
+     * @return Horário do sistema
+     */
     public HorarioSistema qualHorario(){
         return Sistema.getHorario();
     }

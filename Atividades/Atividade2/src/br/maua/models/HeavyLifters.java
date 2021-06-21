804e8bf7f5b61e609ead42b1ee7acec7f9031414
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:33:09 -0300

    Documentação da classe HeavyLifters

--- Atividades/Atividade2/src/br/maua/models/HeavyLifters.java
+++ Atividades/Atividade2/src/br/maua/models/HeavyLifters.java
@@ -3,17 +3,32 @@ package br.maua.models;
 import br.maua.enums.HorarioSistema;
 import br.maua.enums.TiposMembros;
 
+/**
+ * Classe do tipo de função Heavy Lifters
+ */
 public class HeavyLifters extends Membros{
 
+    /**
+     * Método construtor da classe Heavy Lifters
+     * @param nomeUsuario Nome de usuário do membro
+     * @param email e-mail do membro
+     * @param funcao Função do membro
+     */
     public HeavyLifters(String nomeUsuario, String email, TiposMembros funcao) {
         super(nomeUsuario, email, funcao);
     }
 
+    /**
+     * Método que faz a apresentação do membro
+     */
     @Override
     public void apresentar() {
         System.out.println(toString());
     }
 
+    /**
+     * Método que posta uma mensagem dependendo do horário do sistema
+     */
     @Override
     public void postarMensagem() {
         if(qualHorario() == HorarioSistema.Regular)
@@ -22,6 +37,10 @@ public class HeavyLifters extends Membros{
             System.out.println("N00b_qu3_n_Se_r3pita.bat");
     }
 
+    /**
+     * Método toString
+     * @return A apresentação dos dados do membro
+     */
     @Override
     public String toString() {
         return "HeavyLifters{" +

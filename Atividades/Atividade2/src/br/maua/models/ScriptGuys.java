f7ab24e700312c960cd9fd3f5a8931ca74f7a294
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:36:05 -0300

    Documentação da classe ScriptGuys

--- Atividades/Atividade2/src/br/maua/models/ScriptGuys.java
+++ Atividades/Atividade2/src/br/maua/models/ScriptGuys.java
@@ -3,17 +3,32 @@ package br.maua.models;
 import br.maua.enums.HorarioSistema;
 import br.maua.enums.TiposMembros;
 
+/**
+ * Classe do tipo de função Script Guys
+ */
 public class ScriptGuys extends Membros{
 
+    /**
+     * Método construtor da classe Mobile Members
+     * @param nomeUsuario Nome de usuário do membro
+     * @param email e-mail do membro
+     * @param funcao Função do membro
+     */
     public ScriptGuys(String nomeUsuario, String email, TiposMembros funcao) {
         super(nomeUsuario, email, funcao);
     }
 
+    /**
+     * Método que faz a apresentação do membro
+     */
     @Override
     public void apresentar() {
         System.out.println(toString());
     }
 
+    /**
+     * Método que posta uma mensagem dependendo do horário do sistema
+     */
     @Override
     public void postarMensagem() {
         if(qualHorario() == HorarioSistema.Regular)
@@ -22,6 +37,10 @@ public class ScriptGuys extends Membros{
             System.out.println("QU3Ro_S3us_r3curs0s.py");
     }
 
+    /**
+     * Método toString
+     * @return A apresentação dos dados do membro
+     */
     @Override
     public String toString() {
         return "ScriptGuys{" +

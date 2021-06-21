a2f8379a9715d647b8cb25d4efc04ff2a4b7e07e
matheusgalbiati <matheusgalbiati00@gmail.com>
2021-06-21 19:29:45 -0300

    Documentação da classe MobileMembers

--- Atividades/Atividade2/src/br/maua/models/MobileMembers.java
+++ Atividades/Atividade2/src/br/maua/models/MobileMembers.java
@@ -3,17 +3,32 @@ package br.maua.models;
 import br.maua.enums.HorarioSistema;
 import br.maua.enums.TiposMembros;
 
+/**
+ * Classe do tipo de função Mobile Members
+ */
 public class MobileMembers extends Membros{
 
+    /**
+     * Método construtor da classe Mobile Members
+     * @param nomeUsuario Nome de usuário do membro
+     * @param email e-mail do membro
+     * @param funcao Função do membro
+     */
     public MobileMembers(String nomeUsuario, String email, TiposMembros funcao) {
         super(nomeUsuario, email, funcao);
     }
 
+    /**
+     * Método que faz a apresentação do membro
+     */
     @Override
     public void apresentar() {
         System.out.println(toString());
     }
 
+    /**
+     * Método que posta uma mensagem dependendo do horário do sistema
+     */
     @Override
     public void postarMensagem() {
         if(Sistema.getHorario() == HorarioSistema.Regular)
@@ -22,6 +37,10 @@ public class MobileMembers extends Membros{
             System.out.println("Happy_C0d1ng.Maskers");
     }
 
+    /**
+     * Método toString
+     * @return A apresentação dos dados do membro
+     */
     @Override
     public String toString() {
         return "MobileMembers{" +
